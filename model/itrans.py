3962d1233ac2e9dad4058524239c622d9a413c7d
300300 <tairan.chen.ct@hitachiastemo.com>
2023-12-05 18:39:20 +0800

    sine activation in itrans

--- model/itrans.py
+++ model/itrans.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn, optim
 
+from time2vec import SineActivation
+
 
 class ITransformerBlock(nn.Module):
     def __init__(self, variate_num, token_dim, heads, *args, **kwargs) -> None:
@@ -44,17 +46,10 @@ class Model(nn.Module):
             nn.Linear(128, token_dim),
             nn.ReLU(),
         )
-        self.mlp_cond = nn.Sequential(
-            nn.Linear(predict_length, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, token_dim),
-            nn.ReLU(),
+        self.t2v = SineActivation(predict_length, token_dim)
+        self.itrans_blocks = nn.ModuleList(
+            [ITransformerBlock(variate_num, token_dim, heads) for _ in range(block_num)]
         )
-        self.itrans_blocks = nn.ModuleList([
-            ITransformerBlock(variate_num, token_dim, heads) for _ in range(block_num)
-        ])
         self.mlp_transform = nn.Sequential(
             nn.Linear(variate_num + condition_num, 128),
             nn.ReLU(),
@@ -79,7 +74,7 @@ class Model(nn.Module):
         # x: (batch, N, T)
         # z: (batch, M, S)
         x = self.mlp(x)
-        z = self.mlp_cond(z)
+        z = self.t2v(z)
         # x: (batch, N, D)
         # z: (batch, M, D)
         for block in self.itrans_blocks:
